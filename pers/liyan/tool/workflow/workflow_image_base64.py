e65595b47b60a45dc7984fa9ee00af3738a501b0
custsly <2508784442@qq.com>
2021-10-27 11:49:34 +0800

    剪贴板图片转base64 markdown

--- pers/liyan/tool/workflow/workflow_image_base64.py
+++ pers/liyan/tool/workflow/workflow_image_base64.py
@@ -28,22 +28,32 @@ def image_bytes_base64_encode(image_bytes):
     :return: 编码后base64 字符串
     """
     if image_bytes is not None:
-        return str(base64.b64encode(image_bytes), 'utf-8')
+        return 'data:image/jpeg;base64,' + str(base64.b64encode(image_bytes), 'utf-8')
 
 
-def build_md_image_tag(image_name, image_base64):
+def build_md_image_without_var(image_name, image_base64):
     """
     构建 markdown 图标标签,
-    创建一个变量, 变量名为图片名称, 变量值为图片的 base64 编码, 不包括头的部分
-    通过变量引入图片
+    不使用变量
     :param image_name: 图片名称
     :param image_base64: base64 编码
     :return:
     """
-    full_image_base64 = 'data:image/jpeg;base64,' + image_base64
-    image_base64_var = r'[{0}]: {1}'.format(image_name, full_image_base64)
-    image_tag = '![{0}][{1}]'.format(image_name, image_name)
-    return image_tag + '\n\n' + image_base64_var
+    return '![{0}]({1})'.format(image_name, image_base64)
+
+
+def build_md_image_with_var(image_name, image_base64):
+    """
+    构建 markdown 图标标签,
+    创建一个变量, 变量名为图片名称, 变量值为图片的 base64 编码
+    通过变量引入图片, 中间是换行
+    :param image_name: 图片名称
+    :param image_base64: base64 编码
+    :return:
+    """
+    image_base64_var = r'[{0}]: {1}'.format(image_name, image_base64)
+    image_with_var = '![{0}][{1}]'.format(image_name, image_name)
+    return image_with_var + '\n\n' + image_base64_var
 
 
 def main():
@@ -54,8 +64,21 @@ def main():
         workflow_util.add_wf_item(wf, title='not image in clipboard', subtitle=None, arg=None)
     else:
         image_name = 'clipboard_%s' % time.strftime("%Y%m%d%H%M%S", time.localtime())
-        image_tag_str = build_md_image_tag(image_name, img_base64)
-        workflow_util.add_wf_item(wf, title=image_name, subtitle='length %s' % len(image_tag_str), arg=image_tag_str)
+
+        md_image_with_var = build_md_image_with_var(image_name, img_base64)
+        md_image_without_var = build_md_image_without_var(image_name, img_base64)
+
+        # 使用变量插入图片
+        workflow_util.add_wf_item(wf, title=image_name,
+                                  subtitle='md base64 image use var, length %s' % len(md_image_with_var),
+                                  arg=md_image_with_var)
+        # 不使用变量插入图片
+        workflow_util.add_wf_item(wf, title=image_name,
+                                  subtitle='md base64 image not var, length %s' % len(md_image_without_var),
+                                  arg=md_image_without_var)
+        # 仅有图片的base64字符串
+        workflow_util.add_wf_item(wf, title=image_name, subtitle='only image base64, length %s' % len(img_base64),
+                                  arg=img_base64)
 
     wf.send_feedback()
 
