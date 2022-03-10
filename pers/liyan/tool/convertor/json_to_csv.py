712bfeab0de21ac0c7fc6d59373da10630b6580d
custsly <2508784442@qq.com>
2022-03-10 11:58:29 +0800

    convertor

---
+++ pers/liyan/tool/convertor/json_to_csv.py
@@ -0,0 +1,28 @@
+import argparse
+import csv
+import json
+
+parser = argparse.ArgumentParser(description='Convert JSON to CSV')
+
+parser.add_argument('-i', '--input', help='Input JSON file', type=str, required=True)
+parser.add_argument('-o', '--output', help='Output CSV file', type=str, required=True)
+
+
+def convert_json_to_csv(input_json_path, output_csv_path):
+    with open(input_json_path) as json_file:
+        json_data = json.load(json_file)
+
+    with open(output_csv_path, 'w', newline='') as csv_file:
+        csv_writer = csv.writer(csv_file)
+
+        for index, data in enumerate(json_data):
+            if not index:
+                header = data.keys()
+                csv_writer.writerow(header)
+            else:
+                csv_writer.writerow(data.values())
+
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    convert_json_to_csv(args.input, args.output)
---
+++ pers/liyan/tool/convertor/json_to_xlsx.py
@@ -0,0 +1,31 @@
+import argparse
+import json
+
+import pandas as pd
+
+parser = argparse.ArgumentParser(description='Convert JSON to XLSX')
+
+parser.add_argument('-i', '--input', help='Input JSON file', type=str, required=True)
+parser.add_argument('-o', '--output', help='Output XLSX file', type=str, required=True)
+
+
+def convert_json_to_xlsx(input_json_path, output_xlsx_path):
+    with open(input_json_path) as json_file:
+        json_data = json.load(json_file)
+
+    all_rows = []
+    column_list = None
+
+    for index, data in enumerate(json_data):
+        if not index:
+            column_list = data.keys()
+        else:
+            all_rows.append(data.values())
+
+    df = pd.DataFrame(all_rows, columns=column_list)
+    df.to_excel(output_xlsx_path, index=False, engine='xlsxwriter')
+
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    convert_json_to_xlsx(args.input, args.output)
