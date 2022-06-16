31feca7d5552b27deeb722aef79c42cf89c10549
custsly <2508784442@qq.com>
2022-06-16 19:42:27 +0800

    随机图片获取

---
+++ pers/liyan/tool/workflow/random_image_file.py
@@ -0,0 +1,19 @@
+import os
+import random
+import sys
+
+
+def image_file_filter(file_path):
+    name_ext = os.path.splitext(os.path.basename(file_path))
+    image_ext = name_ext[-1]
+    if image_ext.lower() in ['.jpg', '.png', '.bmp']:
+        return True
+    else:
+        return False
+
+
+if __name__ == '__main__':
+    dir_path = sys.argv[1]
+    dir_file_list = os.listdir(dir_path)
+    image_file_list = list(filter(image_file_filter, dir_file_list))
+    print(os.path.abspath(random.choice(image_file_list)), end='')
