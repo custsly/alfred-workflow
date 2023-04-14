abc1b2fa74ba28c9887c09d4787288ea727e2c50
custsly <2508784442@qq.com>
2023-04-14 14:11:01 +0800

    json数组转换

--- pers/liyan/tool/convertor/json_to_csv.py
+++ pers/liyan/tool/convertor/json_to_csv.py
@@ -7,15 +7,32 @@ parser = argparse.ArgumentParser(description='Convert JSON to CSV')
 parser.add_argument('-i', '--input', help='Input JSON file', type=str, required=True)
 parser.add_argument('-o', '--output', help='Output CSV file', type=str, required=True)
 
+def get_json_arr(json_data):
+    """
+    从 json 中取 json 数组
+    """
+    if isinstance(json_data, list):
+        return json_data
+
+    for key in json_data:
+        val = json_data[key]
+        if isinstance(val, list):
+            return val
+
+    return None
 
 def convert_json_to_csv(input_json_path, output_csv_path):
     with open(input_json_path) as json_file:
         json_data = json.load(json_file)
 
+    json_arr = get_json_arr(json_data)
+    if not json_arr:
+        raise Exception("Not JsonArr And Not Contains JsonArr")
+
     with open(output_csv_path, 'w', newline='') as csv_file:
         csv_writer = csv.writer(csv_file)
 
-        for index, data in enumerate(json_data):
+        for index, data in enumerate(json_arr):
             # 写入标题
             if index == 0:
                 header = data.keys()
--- pers/liyan/tool/convertor/json_to_xlsx.py
+++ pers/liyan/tool/convertor/json_to_xlsx.py
@@ -9,14 +9,33 @@ parser.add_argument('-i', '--input', help='Input JSON file', type=str, required=
 parser.add_argument('-o', '--output', help='Output XLSX file', type=str, required=True)
 
 
+def get_json_arr(json_data):
+    """
+    从 json 中取 json 数组
+    """
+    if isinstance(json_data, list):
+        return json_data
+
+    for key in json_data:
+        val = json_data[key]
+        if isinstance(val, list):
+            return val
+
+    return None
+
+
 def convert_json_to_xlsx(input_json_path, output_xlsx_path):
     with open(input_json_path) as json_file:
         json_data = json.load(json_file)
 
+    json_arr = get_json_arr(json_data)
+    if not json_arr:
+        raise Exception("Not JsonArr And Not Contains JsonArr")
+
     all_rows = []
     column_list = None
 
-    for index, data in enumerate(json_data):
+    for index, data in enumerate(json_arr):
         if index == 0:
             column_list = data.keys()
         all_rows.append(data.values())
